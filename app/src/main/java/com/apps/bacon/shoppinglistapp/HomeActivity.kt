74c04ae2d40b3eb821f049a9d664849350497566
Piotr Piekarski <ppiekarski8@gmail.com>
2021-05-06 21:08:06 +0200

    Add listeners

--- app/src/main/java/com/apps/bacon/shoppinglistapp/HomeActivity.kt
+++ app/src/main/java/com/apps/bacon/shoppinglistapp/HomeActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.apps.bacon.shoppinglistapp.databinding.ActivityHomeBinding
+import com.google.android.material.tabs.TabLayout
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
@@ -12,8 +13,27 @@ class HomeActivity : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        initTabs()
 
+        binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                //TODO: display selected lists
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+
+        })
+
+        binding.addShoppingListButton.setOnClickListener {
+            //TODO: open the form for adding a new list
+        }
+
+        initTabs()
     }
 
     private fun initTabs() {
