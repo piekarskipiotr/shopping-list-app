193ed8bf5fd05b800b55be3b2e01cd0a7b8a6f1e
Piotr Piekarski <ppiekarski8@gmail.com>
2021-05-07 09:38:22 +0200

    Create entities

--- app/src/main/java/com/apps/bacon/shoppinglistapp/data/AppDatabase.kt
+++ app/src/main/java/com/apps/bacon/shoppinglistapp/data/AppDatabase.kt
@@ -5,6 +5,8 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.apps.bacon.shoppinglistapp.data.entities.Grocery
+import com.apps.bacon.shoppinglistapp.data.entities.ShoppingList
 
 @Database(
     entities = [ShoppingList::class, Grocery::class],
---
+++ app/src/main/java/com/apps/bacon/shoppinglistapp/data/entities/Grocery.kt
@@ -0,0 +1,24 @@
+package com.apps.bacon.shoppinglistapp.data.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "grocery")
+data class Grocery(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    var id: Int,
+
+    @ColumnInfo(name = "name")
+    var name: String,
+
+    @ColumnInfo(name = "pieces")
+    var pieces: Int,
+
+    @ColumnInfo(name = "is_done")
+    var isDone: Boolean,
+
+    @ColumnInfo(name = "shopping_list_id")
+    var shoppingListId: Int
+)
