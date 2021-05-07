2f5be0a1c41b3913008aa12a62dd57ea2925ee75
Piotr Piekarski <ppiekarski8@gmail.com>
2021-05-07 09:51:27 +0200

    Create repositories

---
+++ app/src/main/java/com/apps/bacon/shoppinglistapp/data/repository/ShoppingListRepository.kt
@@ -0,0 +1,19 @@
+package com.apps.bacon.shoppinglistapp.data.repository
+
+import com.apps.bacon.shoppinglistapp.data.AppDatabase
+import com.apps.bacon.shoppinglistapp.data.entities.ShoppingList
+import javax.inject.Inject
+
+class ShoppingListRepository @Inject constructor(
+    private val database: AppDatabase
+) {
+    fun getActiveShoppingLists() = database.shoppingListDao().getActiveShoppingLists()
+
+    fun getArchivedShoppingLists() = database.shoppingListDao().getArchivedShoppingLists()
+
+    suspend fun insert(shoppingList: ShoppingList) = database.shoppingListDao().insert(shoppingList)
+
+    suspend fun update(shoppingList: ShoppingList) = database.shoppingListDao().update(shoppingList)
+
+    suspend fun delete(shoppingList: ShoppingList) = database.shoppingListDao().delete(shoppingList)
+}
