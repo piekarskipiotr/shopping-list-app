0f5b2410e9036cb81f11a0f08b197712733cc62f
Piotr Piekarski <ppiekarski8@gmail.com>
2021-05-07 09:37:39 +0200

    Create database

---
+++ app/src/main/java/com/apps/bacon/shoppinglistapp/data/AppDatabase.kt
@@ -0,0 +1,38 @@
+package com.apps.bacon.shoppinglistapp.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+
+@Database(
+    entities = [ShoppingList::class, Grocery::class],
+    version = 1,
+    exportSchema = false
+)
+@TypeConverters(DateConverter::class)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun shoppingListDao(): ShoppingListDao
+    abstract fun groceryDao(): GroceryDao
+
+    companion object {
+        @Volatile
+        private var instance: AppDatabase? = null
+        private const val DATABASE_NAME: String = "shopping_app_database"
+
+        @Synchronized
+        fun getInstance(context: Context): AppDatabase {
+            if (instance == null)
+                instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    DATABASE_NAME
+                )
+                    .fallbackToDestructiveMigration()
+                    .allowMainThreadQueries()
+                    .build()
+            return instance!!
+        }
+    }
+}
