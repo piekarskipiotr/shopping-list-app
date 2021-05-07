37bda6f056fe271178aa2b7730f6b548c73758d8
Piotr Piekarski <ppiekarski8@gmail.com>
2021-05-07 15:09:16 +0200

    Init recycler view and display lists

--- app/src/main/java/com/apps/bacon/shoppinglistapp/HomeActivity.kt
+++ app/src/main/java/com/apps/bacon/shoppinglistapp/HomeActivity.kt
@@ -1,22 +1,68 @@
 package com.apps.bacon.shoppinglistapp
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.apps.bacon.shoppinglistapp.adapters.ShoppingListsAdapter
 import com.apps.bacon.shoppinglistapp.databinding.ActivityHomeBinding
+import com.apps.bacon.shoppinglistapp.viewmodel.HomeViewModel
 import com.google.android.material.tabs.TabLayout
+import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
 
-class HomeActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class HomeActivity : AppCompatActivity(), ShoppingListsAdapter.OnShoppingListClickListener {
     private lateinit var binding: ActivityHomeBinding
+    private lateinit var shoppingListAdapter: ShoppingListsAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        val homeViewModel: HomeViewModel by viewModels()
+        initRecyclerView()
 
-        binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+
+        binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab?) {
-                //TODO: display selected lists
+                if (tab != null) {
+                    when(tab.position){
+                        0 -> {
+                            homeViewModel.getArchivedShoppingLists().removeObservers(this@HomeActivity)
+                            homeViewModel.getActiveShoppingLists().observe(this@HomeActivity, {
+                                val activeShoppingListsGroceryNumbers: MutableList<MutableList<Int>> = mutableListOf()
+                                for (shoppingList in it){
+                                    val list = mutableListOf(
+                                        homeViewModel.getNumberOfAllGroceryInShoppingList(shoppingList.id),
+                                        homeViewModel.getNumberOfDoneGroceryInShoppingList(shoppingList.id)
+                                    )
+                                    activeShoppingListsGroceryNumbers.add(list)
+                                }
+                                shoppingListAdapter.updateData(it)
+                                shoppingListAdapter.updateGroceriesNumbers(activeShoppingListsGroceryNumbers)
+                            })
+                        }
+
+                        1 -> {
+                            homeViewModel.getActiveShoppingLists().removeObservers(this@HomeActivity)
+                            homeViewModel.getArchivedShoppingLists().observe(this@HomeActivity, {
+                                val archivedShoppingListsGroceryNumbers: MutableList<MutableList<Int>> = mutableListOf()
+                                for (shoppingList in it){
+                                    val list = mutableListOf(
+                                        homeViewModel.getNumberOfAllGroceryInShoppingList(shoppingList.id),
+                                        homeViewModel.getNumberOfDoneGroceryInShoppingList(shoppingList.id)
+                                    )
+                                    archivedShoppingListsGroceryNumbers.add(list)
+                                }
+                                shoppingListAdapter.updateData(it)
+                                shoppingListAdapter.updateGroceriesNumbers(archivedShoppingListsGroceryNumbers)
+                            })
+                        }
+                    }
+                }
             }
 
             override fun onTabUnselected(tab: TabLayout.Tab?) {
@@ -36,6 +82,19 @@ class HomeActivity : AppCompatActivity() {
         initTabs()
     }
 
+    private fun initRecyclerView() {
+        binding.recyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(context)
+            shoppingListAdapter = ShoppingListsAdapter(this@HomeActivity, this@HomeActivity)
+            adapter = shoppingListAdapter
+            val itemDecoration = DividerItemDecoration(this.context, DividerItemDecoration.VERTICAL).apply {
+                setDrawable(ContextCompat.getDrawable(this@HomeActivity, R.drawable.divider)!!)
+            }
+            addItemDecoration(itemDecoration)
+        }
+    }
+
     private fun initTabs() {
         val listOfTitles =
             listOf(getString(R.string.shopping_lists), getString(R.string.archived_shopping_lists))
@@ -52,5 +111,9 @@ class HomeActivity : AppCompatActivity() {
             }
             binding.tabLayout.addTab(tab, i, isSelected)
         }
+    }
+
+    override fun onShoppingListClick(shoppingListId: Int) {
+        //TODO: start activity
     }
 }
