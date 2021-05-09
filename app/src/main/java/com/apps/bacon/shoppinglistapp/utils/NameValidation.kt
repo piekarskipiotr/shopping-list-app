10220c18c93d9e1ed522bcb25b494f2a7d9db54c
Piotr Piekarski <ppiekarski8@gmail.com>
2021-05-09 14:36:27 +0200

    Update regex pattern

--- app/src/main/java/com/apps/bacon/shoppinglistapp/utils/NameValidation.kt
+++ app/src/main/java/com/apps/bacon/shoppinglistapp/utils/NameValidation.kt
@@ -10,6 +10,6 @@ Name validation:
 object NameValidation {
     fun validate(charSequence: CharSequence?): Boolean {
         val name = charSequence?.trim()
-        return !(name.isNullOrEmpty() || !name.contains(Regex("[A-Za-z]")) || name.contains(Regex("[^A-Za-z0-9\\s]")))
+        return !(name.isNullOrEmpty() || !name.contains(Regex("[A-Za-z\\p{L}]")) || name.contains(Regex("[^A-Za-z0-9\\s\\p{L}]")))
     }
 }
