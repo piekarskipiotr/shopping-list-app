47ace08d9855f75acf462b841116ba7ae127697d
Piotr Piekarski <ppiekarski8@gmail.com>
2021-05-13 19:02:11 +0200

    Remove timer of splash screen

--- app/src/main/java/com/apps/bacon/shoppinglistapp/ui/MainActivity.kt
+++ app/src/main/java/com/apps/bacon/shoppinglistapp/ui/MainActivity.kt
@@ -1,23 +1,15 @@
 package com.apps.bacon.shoppinglistapp.ui
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
 import com.apps.bacon.shoppinglistapp.ui.home.HomeActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Handler(Looper.getMainLooper()).postDelayed({
-            intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
-            finish()
-        }, DELAY)
-    }
-
-    companion object {
-        const val DELAY: Long = 1000
+        intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
